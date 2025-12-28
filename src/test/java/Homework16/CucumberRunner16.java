25f2d0f4724af1ac01715ab9862e5fceea0aec28
LadykaAlisa <ladykaalisa@gmail.com>
2025-12-28 19:13:02 +0200

    fix

--- src/test/java/Homework16/CucumberRunner16.java
+++ src/test/java/Homework16/CucumberRunner16.java
@@ -1,8 +1,7 @@
-package Homework15;
+package Homework16;
 
 
 import homework15.DBConnectionFactory;
-import homework15.EnvType;
 import homework15.WebDriverFactory;
 import homework_pom.MainPage;
 import homework_pom.SearchResultsPage;
@@ -17,42 +16,47 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 
-
 @CucumberOptions(
         features = "src/test/resources/features13",
         glue = "Homework13.steps",
         plugin = {
                 "pretty",
-                "html:target/cucumber-reports/cucumber15.html",
-                "json:target/cucumber-reports/cucumber15.json",
+                "html:target/cucumber-reports/cucumber16.html",
+                "json:target/cucumber-reports/cucumber16.json",
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
         }
 )
-public class CucumberRunner15 extends AbstractTestNGCucumberTests {
-    private final static DBConnectionFactory connectionFactory = new DBConnectionFactory();
+public class CucumberRunner16 extends AbstractTestNGCucumberTests {
+    private final static WebDriverFactory driverFactory = new WebDriverFactory();
+    private  final static DBConnectionFactory connectionFactory = new DBConnectionFactory();
+    private WebDriver driver;
     private Connection conn;
+
+    private MainPage page;
+    private SearchResultsPage searchResultsPage;
 
     @BeforeSuite
     public void beforeSuite() throws SQLException, MalformedURLException {
         // WebDriver будет инициализирован в CucumberHooks
         // Эти переменные могут быть инициализированы позже, когда WebDriver будет готов
-        // driver = WebDriverFactory.getDriver(); // Commented out as it's managed by CucumberHooks
+        driver = WebDriverFactory.getDriver();
+        page = null;
+        searchResultsPage = null;
         conn = connectionFactory.getConnection();
+
+
+//        conn = DriverManager.getConnection(
+//                "jdbc:mysql://localhost:3306/db?useUnicode=true&characterEncoding=UTF-8",
+//                "root",
+//                "11111111"
+//        );
+//        stmt = conn.createStatement();
     }
 
     @AfterSuite
     public void tearDown() {
         // WebDriver управляется CucumberHooks, поэтому здесь ничего не делаем
         // CucumberHooks.@After закроет браузер
-        
-        // Close database connection
-        if (conn != null) {
-            try {
-                conn.close();
-            } catch (SQLException e) {
-                System.err.println("Error closing database connection: " + e.getMessage());
-            }
-        }
     }
 }
 
